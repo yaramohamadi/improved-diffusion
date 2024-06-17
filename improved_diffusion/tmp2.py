564813ec60ba1048579702e00b7b0641f9f3ec59
Yara M. Bahram <yara.mohamadI@gmail.com>
2024-06-17 10:36:26 -0400

    Add files via upload

---
+++ improved_diffusion/tmp2.py
@@ -0,0 +1,7 @@
+import evaluation
+
+ref_path = '/home/ymbahram/scratch/pokemon/pokemon_64x64.npz'
+sample_path = '/home/ymbahram/projects/def-hadi87/ymbahram/improved_diffusion/results/samples/samples_0.npz'
+evaluation.runEvaluate(ref_path, sample_path, verbose=True)
+
+print('excuse me wtf')
