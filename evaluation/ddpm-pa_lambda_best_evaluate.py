92e1a1b66aadf0785837a86794f0e7d2b712e3e2
yaramohamadi <yara.mohamadi@gmail.com>
2025-01-17 14:53:54 -0500

    .

--- evaluation/ddpm-pa_lambda_best_evaluate.py
+++ evaluation/ddpm-pa_lambda_best_evaluate.py
@@ -1,3 +1,7 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
 import pandas as pd
 import numpy as np
 import importlib
@@ -5,13 +9,16 @@ import importlib
 import evaluation 
 importlib.reload(evaluation)
 
-ref_path = '/home/ymbahram/scratch/pokemon/pokemon_64x64.npz' # The target full dataset
-target_path = '/home/ymbahram/scratch/pokemon/pokemon_10.npz' # The target 10-shot dataset
-source_batch = '/home/ymbahram/projects/def-hadi87/ymbahram/improved_diffusion/util_files/imagenet_pretrained.npz' # Source samples from pre-fixed noise vectors
+# ref_path = '/home/ymbahram/scratch/pokemon/pokemon_64x64.npz' # The target full dataset
+ref_path = '/export/livia/home/vision/Ymohammadi/datasets/pokemon/pokemon_64x64.npz' # The target full dataset
+# target_path = '/home/ymbahram/scratch/pokemon/pokemon_10.npz' # The target 10-shot dataset
+target_path = '/export/livia/home/vision/Ymohammadi/datasets/pokemon/pokemon_10.npz' # The target 10-shot dataset
+# source_batch = '/home/ymbahram/projects/def-hadi87/ymbahram/improved_diffusion/util_files/imagenet_pretrained.npz' # Source samples from pre-fixed noise vectors
+source_batch = '/export/livia/home/vision/Ymohammadi/util_files/imagenet_pretrained.npz' # Source samples from pre-fixed noise vectors
 
 g = 0
 
-for repetition in range(3):
+for repetition in range(1, 3):
 
     for p2_gamma in [0]: # TODO Quoi??
 
@@ -23,14 +30,17 @@ for repetition in range(3):
 
                     for dataset_size in [10]:
 
-                        file_path = f"/home/ymbahram/scratch/baselines_avg/ddpm-pa/data{dataset_size}/FID_KID_LPIPS_cosine.csv"
+                        # file_path = f"/home/ymbahram/scratch/baselines_avg/ddpm-pa/data{dataset_size}/FID_KID_LPIPS_cosine.csv"
+                        file_path = f"/export/livia/home/vision/Ymohammadi/baselines_avg/ddpm-pa/FID_KID_LPIPS_cosine.csv"
 
                         for epoch in [0, 25, 50, 75, 100, 125, 150, 175, 200]:
                             
                             print("*"*20)
                             print(f"g {g} gamma {p2_gamma} lambda_1 {lambda_1} lambda_2 {lambda_2} lambda_3 {lambda_3} configuration {epoch} epoch")
                             print("*"*20)   
-                            sample_path = f"/home/ymbahram/scratch/baselines_avg/ddpm-pa/data{dataset_size}/l1_{lambda_1}_l2_{lambda_2}_l3_{lambda_3}_repeat_{repetition}/samples/samples_{epoch}.npz"
+                            # sample_path = f"/home/ymbahram/scratch/baselines_avg/ddpm-pa/data{dataset_size}/l1_{lambda_1}_l2_{lambda_2}_l3_{lambda_3}_repeat_{repetition}/samples/samples_{epoch}.npz"
+                            sample_path = f"/export/livia/home/vision/Ymohammadi/baselines_avg/ddpm-pa/l1_{lambda_1}_l2_{lambda_2}_l3_{lambda_3}_repeat_{repetition}/samples/samples_{epoch}.npz"
+                            
                             results = evaluation.runEvaluate(ref_path, sample_path, 
                                                 FID=True, 
                                                 #IS=True, 
